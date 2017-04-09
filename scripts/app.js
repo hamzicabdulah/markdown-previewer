eb1e7e3d1d0ea24574c51458d1ce5c9107c26fa1
hamzicabdulah <hamzic@live.com>
2017-04-09 16:17:30 +0200

    Now requiring React and ReactDOM, which was the actual 'bug'

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+const marked = require('marked');
 let Markdown = React.createClass({
   getInitialState: function () {
     const initialResult = 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*';
